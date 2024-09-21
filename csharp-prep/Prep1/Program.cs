7b2b442edc4135e011fc4372ae832f2c25ef82c9
ericselace <eriselace@gmail.com>
2024-09-21 21:58:26 +0000

    name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("please write your first name ");
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine("what is family name?");
+        string familyname = Console.ReadLine();
+
+        Console.WriteLine($"your name {firstname} {familyname}");
     }
 }
