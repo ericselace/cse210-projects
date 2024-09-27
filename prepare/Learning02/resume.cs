7fe2c58f31b3a71d4aa775c144704e67e3af9586
ericselace <eriselace@gmail.com>
2024-09-27 09:00:41 +0000

    m

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -2,5 +2,15 @@ public class Resume
 {
    public string _Name ;
    public string _Prenoms; 
-   public string _Job
+   public List<Job> _Jobs= new   List<Job>();
+   public void Display()
+   {
+     Console.WriteLine($"name: {_Name} {_Prenoms}");
+     Console.WriteLine("Jobs:");
+      foreach (Job job in _Jobs)
+      {
+         job.Display();
+      }
+     
+   }
 }
